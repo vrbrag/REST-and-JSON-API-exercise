88daa2d454fcc359ed5634986c58ae0d520dc098
Vanessa Bragdon <vanessabragdon@Vanessas-MBP.home>
2022-08-10 20:46:59 -0700

    create cupcake model

--- models.py
+++ models.py
@@ -1,1 +1,26 @@
 """Models for Cupcake app."""
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import null
+
+db = SQLAlchemy()
+
+DEFAULT_IMG =  'https://tinyurl.com/demo-cupcake'
+class Cupcake(db.Model):
+   """Cupcakes"""
+
+   __tablename__= "cupcakes"
+
+   id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+   flavor = db.Column(db.Text, nullable = False)
+   size = db.Column(db.Text, nullable = False)
+   rating = db.Column(db.Float, nullable = False)
+   image = db.Column(db.Text)
+
+   def image_url(self):
+      """Return image for cupcake"""
+
+      return self.image or DEFAULT_IMG
+
+def connect_db(app):
+   db.app = app
+   db.init_app(app)
